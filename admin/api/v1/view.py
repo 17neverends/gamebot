3a1a7ab788ce05e6a8c28733747be84695482f0b
17neverends <ionianrise@gmail.com>
2024-11-08 23:48:42 +0300

    add admin

--- admin/api/v1/view.py
+++ admin/api/v1/view.py
@@ -4,12 +4,6 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="build")
 
-
-@router.get("/auth")
-async def auth(req: Request):
-    return templates.TemplateResponse('index.html', {'request': req})
-
-
 @router.get("/database")
 async def database(req: Request):
     return templates.TemplateResponse('index.html', {'request': req})
