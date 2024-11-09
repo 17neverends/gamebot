57710b55da68f08f92912b8837faf2e0a3989388
17neverends <ionianrise@gmail.com>
2024-11-09 17:11:43 +0300

    compose fix

--- admin/api/v1/view.py
+++ admin/api/v1/view.py
@@ -3,6 +3,8 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="/app/dist")
+
+
 @router.get("/database")
 async def database(req: Request):
     return templates.TemplateResponse('index.html', {'request': req})
