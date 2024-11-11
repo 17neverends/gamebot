c3dda9b9825708a6029c6397def11effce237526
17neverends <ionianrise@gmail.com>
2024-11-11 22:50:43 +0300

    fixxx

--- admin/api/v1/endpoints/sudoku.py
+++ admin/api/v1/endpoints/sudoku.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from tasks_shared.database_utils import get_session
@@ -14,11 +15,13 @@ router = APIRouter(
 
 
 @router.get("/get_all", response_class=JSONResponse)
-async def get_all_records(start_date: str, end_date: str):
+async def get_all_records(start_date: Optional[str] = None, end_date: Optional[str] = None):
     async with get_session() as session:
-        sudoku_repo = SudokuRepository(session)
-        start_date = await convert_string_to_datetime(dt=start_date)
-        end_date = await convert_string_to_datetime(dt=end_date)
-        result = await sudoku_repo.get_all(start_date=start_date,
-                                           end_date=end_date)
+        spotting_repo = SudokuRepository(session)
+        if start_date and end_date:
+            start_date = await convert_string_to_datetime(dt=start_date)
+            end_date = await convert_string_to_datetime(dt=end_date)
+            result = await spotting_repo.get_all(start_date=start_date, end_date=end_date)
+        else:
+            result = await spotting_repo.get_all(start_date=start_date, end_date=end_date)
         return result
