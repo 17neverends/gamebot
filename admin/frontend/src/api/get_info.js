6e8770c3cf87c60425e8be77062710a4633447d2
17neverends <ionianrise@gmail.com>
2024-11-11 23:05:13 +0300

    another fix

--- admin/frontend/src/api/get_info.js
+++ admin/frontend/src/api/get_info.js
@@ -10,7 +10,7 @@ export async function get_table_info({table}) {
 }
 
 export async function get_filtered_table_info({ table, start_date, end_date }) {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/${table}/get_filtered?start_date=${encodeURIComponent(start_date)}&end_date=${encodeURIComponent(end_date)}`, {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/${table}/get_all?start_date=${encodeURIComponent(start_date)}&end_date=${encodeURIComponent(end_date)}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
