ae2e603132b3979298e2948653665a959448a2e8
17neverends <ionianrise@gmail.com>
2024-12-04 23:10:12 +0300

    hotfix/import

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,6 @@ from aiogram import Bot, Dispatcher
 from bot.routers.language.commands import router as lang_commands_router
 from bot.routers.language.callbacks import router as lang_callbacks_router
 from bot.routers.games.commands import router as games_commands_router
-from bot.routers.games.callbacks import router as games_callbacks_router
 from settings import settings
 
 
@@ -15,7 +14,6 @@ async def main():
     dp.include_router(lang_commands_router)
     dp.include_router(lang_callbacks_router)
     dp.include_router(games_commands_router)
-    dp.include_router(games_callbacks_router)
     await dp.start_polling(bot)
 
 
