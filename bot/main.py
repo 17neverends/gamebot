7ccf064f7ed24221b4b1961501b06d8c54cb3135
17neverends <ionianrise@gmail.com>
2024-12-04 19:28:10 +0300

    bot game menu

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,21 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from routers.language.commands import router as lang_commands_router
-from routers.language.callbacks import router as lang_callbacks_router
+from bot.routers.language.commands import router as lang_commands_router
+from bot.routers.language.callbacks import router as lang_callbacks_router
+from bot.routers.games.commands import router as games_commands_router
+from bot.routers.games.callbacks import router as games_callbacks_router
 from settings import settings
 
 
-async def main():
+async def main():    
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=settings.bot_token)
     dp = Dispatcher()
     dp.include_router(lang_commands_router)
     dp.include_router(lang_callbacks_router)
+    dp.include_router(games_commands_router)
+    dp.include_router(games_callbacks_router)
     await dp.start_polling(bot)
 
 
