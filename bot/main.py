272f71886973107724d5082f453d533d141b88b9
17neverends <ionianrise@gmail.com>
2024-12-05 18:46:15 +0300

    try fix circular import

--- bot/main.py
+++ bot/main.py
@@ -8,19 +8,19 @@ from bot.routers.games.callbacks import router as games_callbacks_router
 from settings import settings
 from bot.utils.config import set_bot_description
 
-bot = Bot(token=settings.bot_token)
+new_bot = Bot(token=settings.bot_token)
 
 
 async def main():    
     logging.basicConfig(level=logging.INFO)
-    await set_bot_description(bot=bot)
+    await set_bot_description(bot=new_bot)
     dp = Dispatcher()
     dp.include_router(lang_commands_router)
     dp.include_router(lang_callbacks_router)
     dp.include_router(games_commands_router)
     dp.include_router(games_callbacks_router)
 
-    await dp.start_polling(bot)
+    await dp.start_polling(new_bot)
 
 
 if __name__ == "__main__":
