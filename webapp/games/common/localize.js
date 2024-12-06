d6cf13dc09cdfa485ebb3daf7ddf2a4d4a08d57b
17neverends <ionianrise@gmail.com>
2024-12-06 22:46:50 +0300

    test localize adaptation on frontend

---
+++ webapp/games/common/localize.js
@@ -0,0 +1,81 @@
+export const game_name_text = {
+    "tetis": {
+        "ru": "Тетрис",
+        "gb": "Tetris",
+        "in": "टेट्रिस"
+    },
+    "minisweeper": {
+        "ru": "Сапёр",
+        "gb": "Minesweeper",
+        "in": "माइन स्वीपर"
+    },
+    "wordle": {
+        "ru": "Вордли",
+        "gb": "Wordle",
+        "in": "वोर्डल"
+    },
+    "sudoku": {
+        "ru": "Судоку",
+        "gb": "Sudoku",
+        "in": "सुडोकू"
+    },
+    "spotting": {
+        "ru": "Пятнашки",
+        "gb": "Spotting",
+        "in": "स्पॉटिंग"
+    },
+    "tiktaktoe": {
+        "ru": "Крестики-нолики",
+        "gb": "Tic Tac Toe",
+        "in": "टिक टैक टॉइ"
+    }
+}
+
+
+export const congratilations_text = {
+    "ru": "Поздравляем!",
+    "gb": "Congratulations!",
+    "in": "बिलकुल!"
+}
+
+export const new_game_text = {
+    "ru": "Новая игра",
+    "gb": "New game",
+    "in": "नया खेल"
+}
+
+export const start_game_text = {
+    "ru": "Начать игру",
+    "gb": "Start game",
+    "in": "खेल शुरू करें"
+}
+
+export const won_text = {
+    "ru": "победил!",
+    "gb": "won!",
+    "in": "जीता!"
+}
+
+export const draw_text = {
+    "ru": "Ничья!",
+    "gb": "Draw!",
+    "in": "निश्चित!"
+}
+
+export const enemy_win = {
+    "ru": "O победил!",
+    "gb": "O won!",
+    "in": "O जीता!"
+}
+
+export const empty_leaderboard_text = {
+    "ru": "Таблица лидеров пуста",
+    "gb": "Leaderboard is empty",
+    "in": "लीडरबोर्ड खाली है"
+}
+
+export const result_title_text = {
+    "ru": "Результат игры",
+    "gb": "Result game",
+    "in": "परिणाम खेल"
+}
