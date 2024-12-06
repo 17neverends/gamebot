918bf3f76c82132ff0f528ac06091d0b14a792c6
17neverends <ionianrise@gmail.com>
2024-12-06 14:27:07 +0300

    getting instance of bot from callback

--- bot/routers/language/callbacks.py
+++ bot/routers/language/callbacks.py
@@ -1,4 +1,4 @@
-from aiogram import Router, types
+from aiogram import Router, types, Bot
 from bot.routers.language.factory import LanguageCallbackFactory
 from bot.routers.greeting.message import send_greeting
 from tasks_shared.models.user.repository import UserRepository
@@ -7,14 +7,12 @@ from tasks_shared.models.user.schemas import UserUpdate
 from bot.routers.games.commands import send_games
 from bot.utils.config import set_commands
 
-
 router = Router()
 
 
 @router.callback_query(LanguageCallbackFactory.filter())
 async def callbacks_lang_change(callback: types.CallbackQuery, 
                                 callback_data: LanguageCallbackFactory):
-    from bot.main import new_bot
     if callback_data.action == "change":
         update_model = UserUpdate(lang=callback_data.value)
         async with get_session() as session:
@@ -22,7 +20,7 @@ async def callbacks_lang_change(callback: types.CallbackQuery,
             updated_user = await repository.update_user_by_tg_id(tg_id=callback.from_user.id,
                                                                  user_update=update_model)
     if updated_user:
-        await set_commands(bot=new_bot, language=updated_user.lang)
+        await set_commands(bot=callback.bot, language=updated_user.lang)
         await callback.message.delete()
         await send_greeting(callback.message, updated_user.lang)
         await send_games(callback.message, updated_user.lang)
