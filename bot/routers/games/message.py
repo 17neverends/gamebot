572e1c1d0b53011a4cc35f6bcf5690dde093e52f
17neverends <ionianrise@gmail.com>
2024-12-06 15:14:56 +0300

    adding photo to answer

--- bot/routers/games/message.py
+++ bot/routers/games/message.py
@@ -1,7 +1,8 @@
 from aiogram import types
 from bot.utils.localize import games_text, games_description
 from bot.routers.games.kb import game_handler
-
+from aiogram.types import FSInputFile
+from settings import settings
 
 async def send_games(message: types.Message, language: str) -> None:
     await message.answer(text=games_text.get(language))
@@ -10,5 +11,12 @@ async def send_games(message: types.Message, language: str) -> None:
 async def send_choose_game(message: types.Message,
                            language: str,
                            game: str) -> None:
-    await message.answer(text=games_description.get(game).get(language),
-                         reply_markup=await game_handler(game=game, language=language))
+    try:
+        game_image = FSInputFile(f"{settings.bot_static_path}{game}.jpeg")
+
+        await message.answer_photo(text=games_description.get(game).get(language),
+                                reply_markup=await game_handler(game=game, language=language),
+                                photo=game_image)
+    except FileNotFoundError:
+        await message.answer(text=games_description.get(game).get(language),
+                                  reply_markup=await game_handler(game=game, language=language))
