b0c4629720fb27052bcced970f979af42772fd38
17neverends <ionianrise@gmail.com>
2024-12-06 23:16:23 +0300

    import fix

--- webapp/games/tiktaktoe/main.js
+++ webapp/games/tiktaktoe/main.js
@@ -1,5 +1,5 @@
-import { lang } from "/common/lang.js";
-import { empty_leaderboard_text, enemy_win, draw_text, won_text, game_name_text, seconds_text,  welcome_text } from "/common/localize.js";
+import { lang } from "/games/common/lang.js";
+import { empty_leaderboard_text, enemy_win, draw_text, won_text, game_name_text, seconds_text,  welcome_text } from "/games/common/localize.js";
 const gameName = "tiktaktoe";
 document.title = game_name_text[gameName][lang];
 
