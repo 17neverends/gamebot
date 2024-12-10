4f444d6d57b771a076a9dfebee0253d872f3f3f3
17neverends <ionianrise@gmail.com>
2024-12-10 22:06:54 +0300

    wordle localization

--- webapp/games/common/rules.js
+++ webapp/games/common/rules.js
@@ -1,17 +1,14 @@
-import { enter_text, delete_text, rules_points_text, continue_text, rules_text } from "./localize.js";
+import { rules_points_text, continue_text, rules_text } from "./localize.js";
 
 
 const urlParams = new URLSearchParams(window.location.search);
 export const lang = urlParams.get('lang');
 
 
-const enterButton = document.getElementById('enter');
-const delButton = document.getElementById('del');
 const rulesTitle = document.getElementById('rulesTitle');
 const continueTitle = document.getElementById('continueGame');
 
-enterButton.textContent = enter_text[lang];
-delButton.textContent = delete_text[lang];
+
 rulesTitle.textContent = rules_text[lang];
 continueTitle.textContent = continue_text[lang];
 
